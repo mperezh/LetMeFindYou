fd6a031c9638d4ae675eac1efe045e5658dc8c2d
Manuel <manuelph19@gmail.com>
2017-04-08 17:30:56 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+import { headerComponent } from './header/app.headerComponent';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './main.html',
+})
+export class AppComponent  { name = 'Angular'; }
---
+++ src/app/header/app.headerComponent.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by manuelph on 4/8/17.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header',
+  templateUrl: './main.html',
+})
+export class headerComponent  {
+
+}
